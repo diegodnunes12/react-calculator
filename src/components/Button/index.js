6433b6d298cb97721c35054290553a5ba8d8bccb
Diego Nunes <diegodnunes12@gmail.com>
2023-11-25 18:37:57 -0300

    Criando estrutura da calculadora

---
+++ src/components/Button/index.js
@@ -0,0 +1,11 @@
+const Button = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello World
+        </header>
+      </div>
+    );
+  }
+  
+  export default Button;
---
+++ src/components/Input/index.js
@@ -0,0 +1,11 @@
+const Input = () => {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Hello World
+        </header>
+      </div>
+    );
+  }
+  
+  export default Input;
